fcad9f40ffdb87d575977da8c1d943781fd7fe80
liron deri <liron.deri3@outlook.com>
2023-08-29 13:29:15 +0300

    adding changes 29.8.23 13:30

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from flask import Flask, render_template
-#from flaskext.mysql import MySQL
-#from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
+from bson import json_util
+from bson import ObjectId
 app = Flask(__name__)
-#app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-#app.config['MYSQL_DATABASE_DB'] = 'EmpData'
-#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-#app.config['MYSQL_DATABASE_URI'] = 'mysql://list.db'
-#db = MySQL(app)
-#db.init_app(app)
+try:
+    client = MongoClient('mongodb://localhost:27017/')  
 
-#class Item(db.Model):
-   # id = db.Column(db.Integer(), primary_key=True)
-   # item = db.Column(db.Integer(), nullable=False)
-   # type = db.Column(db.String(length=30), nullable=False, unique=True)
-   # quantity = db.Column(db.Integer(), nullable=False)
-
+    db = client['Website_db']
+    print("mongo connect")
+except Exception:
+    print("enable to connect mongodb")
+db.users.insert_one({"name": "John", "email": "john@example.com"})
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -29,5 +24,10 @@ def list_page():
         {'id': 3, 'type': 'Keyboard', 'mark': '4354545', 'price': 300}
     ]
     return render_template('list.html', items=items)
+
+@app.route('/create-accont')
+def create_account():
+    return render_template('create-account.html')
+
 if __name__ == '__main___':
-    app.run()
+  app.run(debug=True, host="0.0.0.0")
