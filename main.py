d64c96ee0eac3ecb9a59c2e61feeb06016779ff4
liron deri <liron.deri3@outlook.com>
2023-08-29 16:19:48 +0300

    djnd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson import json_util
 from bson import ObjectId
@@ -10,18 +10,18 @@ try:
     print("mongo connect")
 except Exception:
     print("enable to connect mongodb")
+
 db.users.insert_one({"name": "John", "email": "john@example.com"})
-@app.route('/')
-@app.route('/home')
+
+@app.route('/', methods=('GET', 'POST'))
+@app.route('/home', methods=('GET', 'POST'))
 def home_page():
     return render_template('index.html')
 
-@app.route('/list')
+@app.route('/list', methods=('GET', 'POST'))
 def list_page():
     items = [
-        {'id': 1, 'type':'Phone','mark':'869767869', 'price':500},
-        {'id': 2, 'type': 'Laptop', 'mark': '764754q34234', 'price': 700},
-        {'id': 3, 'type': 'Keyboard', 'mark': '4354545', 'price': 300}
+       
     ]
     return render_template('list.html', items=items)
 
