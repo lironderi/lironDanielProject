940d82e44d7b343e68f21e49b113fe9d42830648
liron deri <liron.deri3@outlook.com>
2023-08-30 20:28:47 +0300

    end of the day 30.8.23

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 app.secret_key = 'fakekey'
 
+#create DB
 try:
     client = MongoClient('mongodb://localhost:27017/')  
     db = client['Website_db']
@@ -14,11 +15,17 @@ try:
 except Exception:
     print("enable to connect mongodb")
 
+#route to home page
 @app.route('/', methods=('GET', 'POST'))
 @app.route('/home', methods=('GET', 'POST'))
 def home_page():
     return render_template('index.html')
+# about page
+@app.route('/about_page')
+def about_page():
+    return render_template('about.html')
 
+#first list to create after registeration
 @app.route('/first-list', methods=['GET','POST'])
 def first_list_page():
     user = session.get('user')
@@ -29,15 +36,16 @@ def first_list_page():
     if request.method == 'POST':
         # ...
         if item and quantity:
-            user_items_collection = db[user['_id']]  # Use the user's username as collection name
+            user_items_collection = db[user['_id']]  # Use the user's id as collection name
             user_items_collection.insert_one({"item": item, "quantity": quantity})
 
-    user_items_collection = db[user['username']]  # Use the user's username as collection name
+    user_items_collection = db[user['_id']]  # Use the user's id as collection name
     items = user_items_collection.find()
 
     return render_template('first-list.html', user=user, items=items)
 
 
+#insert items to the DB to a collection called by the user id
 @app.route('/save-items', methods=['POST'])
 def save_items():
     user = session.get('user')
@@ -48,23 +56,31 @@ def save_items():
     items = data.get('items')
 
     if items:
-        user_items_collection = db[user['_id']]  # Use the user's username as collection name
+        user_items_collection = db[user['_id']]  # Use the user's id as collection name
         user_items_collection.insert_many(items)
         return redirect(url_for('new_list'))
     else:
         return {"message": "No items provided"}, 400
 
 
+# create new list when you already have an account
 @app.route('/new_list', methods=('GET', 'POST'))
 def new_list_page():
-    return render_template('new_list.html')
-
+    user = session.get('login_user')
+    if user:
+        user_items_collection = db[user['_id']]  # Use the user's id as collection name
+        items = user_items_collection.find()  # Retrieve all items from the collection
+        return render_template('new_list.html', items=items)
+    else:
+        return redirect(url_for('login_page'))
+   
+# registeration page
 @app.route('/create-accont')
 def create_account():
     return render_template('create-account.html')
 
 
-
+# registeration function
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form.get('username')
@@ -88,28 +104,57 @@ def register():
         return "Invalid username or password", 401
     # return redirect(url_for('login_page'))
 
+
+# login page
 @app.route('/login_page')
 def login_page():
     return render_template('login.html')
 
+# function that check if user exist and create a session
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form.get('username')
-    # Hash the password for comparison
+    
     password = hashlib.sha256(request.form.get('password').encode()).hexdigest()
 
-    # Query MongoDB to find the user
+    
     user = db.users.find_one({"username": username, "password": password})
 
     if user:
-        session['username'] = username  # Add the username to the session
+        user['_id'] = str(user['_id'])  # Convert ObjectId to string
+        session['login_user'] = user  # Store the entire user document in the session
         return redirect(url_for('new_list_page'))
     else:
         return "Invalid username or password", 401
+    
+# create temporary collection
+@app.route('/temp_col', methods=['POST'])
+def temp_col():
+    user = session.get('login_user')
+    if user:
+        temp_collection_name = f"temp{db[user['_id']]}"  # Use the _id from the user document
+        temp_collection = db[temp_collection_name]
 
+        data = request.json
+        items = data.get('items')
+
+        if items:
+            documents = [{"item": item["item"], "quantity": item["quantity"]} for item in items]
+            temp_collection.insert_many(documents)
+            return {"message": "Items added to temporary collection"}
+        else:
+            return {"message": "No items provided"}, 400
+    else:
+        return redirect(url_for('login_page'))
+
+# function that taking the user out of the session
 @app.route('/logout')
 def logout():
-    session.pop('username', None)  # Remove username from session
+    user = session.get('login_user')
+    if user:
+        temp_collection_name = f"temp{db[user['_id']]}"
+        db.drop_collection(temp_collection_name)  # Delete the temporary collection
+    session.pop('login_user', None)
     return redirect(url_for('home_page'))
 
 
