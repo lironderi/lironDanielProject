eb41adf21b7828e45ea007bf233cfb1ab7117ed5
liron deri <liron.deri3@outlook.com>
2023-09-21 14:46:39 +0300

    fixing 21/9/23

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect, session
+from flask import Flask, render_template, request, url_for, redirect, session, jsonify
 from pymongo import MongoClient
-import hashlib  # for password hashing
+import hashlib 
 import os
 from bson import ObjectId
 
@@ -12,75 +12,20 @@ try:
     db = client['Website_db']
     print("mongo connect")
 except Exception:
-    print("enable to connect mongodb")
+    print("enable to connect mongodb")
+
+
 #route to home page
 @app.route('/', methods=('GET', 'POST'))
 @app.route('/home', methods=('GET', 'POST'))
 def home_page():
     return render_template('index.html')
+
+
 # about page
 @app.route('/about_page')
 def about_page():
         return render_template('about.html')
-#first list to create after registeration
-@app.route('/first-list', methods=['GET','POST'])
-def first_list_page():
-    user = session.get('user')
-    if not user:
-        return redirect(url_for('register'))
-    item = request.form.get('item')
-    quantity = request.form.get('quantity')
-    if request.method == 'POST':
-        # ...
-        if item and quantity:
-            user_items_collection = db[user['_id']]  # Use the user's id as collection name
-            user_items_collection.insert_one({"item": item, "quantity": quantity})
-            session['login_user'] = user
-
-    return render_template('first-list.html', user=user)
-
-
-#insert items to the DB to a collection called by the user id
-@app.route('/save-items', methods=['POST'])
-def save_items():
-    user = session.get('user')
-    login_user = session.get('login_user')
-        
-    if user:
-        data = request.json
-        items = data.get('items')
-
-        if items:
-            user_items_collection = db[user['_id']]  # Use the user's id as collection name
-            user_items_collection.insert_many(items)
-            session['login_user'] = user
-            return redirect(url_for('my_list'))
-        
-    if login_user:
-        data = request.json
-        current_items = db.db[user['_id']].find()
-        items = data.get('items')
-        if items and current_items:
-            user_items_collection = db[user['_id']]  # Use the user's id as collection name
-            user_items_collection.insert_many(current_items, items)
-            session['login_user'] = user
-            return redirect(url_for('my_list'))
-    
-    return "something"   
-
-
-# create new list when you already have an account
-@app.route('/new_list', methods=('GET', 'POST'))
-def new_list_page():
-    user = session.get( 'login_user')
-    if user:
-        user_items_collection = db[user['_id']]  # Use the user's id as collection name
-        items = user_items_collection.find()  # Retrieve all items from the collection
-        return render_template('new_list.html',user=user, items=items)
-    else:
-        return redirect(url_for('login_page'))
-
-    
 
 # registeration page
 @app.route('/create-accont')
@@ -111,10 +56,11 @@ def register():
     user = db.users.find_one({"username": username, "password": password})
     if user:
         user['_id'] = str(user['_id'])
-        session['user'] = user  # Add the username to the session
-        return redirect(url_for('first_list_page'))
+        session['first_login'] = user  # Add the username to the session
+        return redirect(url_for('login_page'))
 
     return "Invalid username or password", 401
+
 
 # login page
 @app.route('/login_page')
@@ -133,17 +79,46 @@ def login():
 
     if user:
         user['_id'] = str(user['_id'])  # Convert ObjectId to string
-        session['login_user'] = user  # Store the entire user document in the session
-        return redirect(url_for('my_list'))
+        if user['_id'] in db.list_collection_names():
+            session['login_user'] = user  
+        else: 
+            session['first_login'] = user
+        return redirect(url_for('new_list_page'))
     else:
         return "Invalid username or password", 401
+
+
+@app.route('/new_list', methods=('GET', 'POST'))
+def new_list_page():
+    user = session.get('login_user')
+    first = session.get('first_login')
+    if first:
+        if request.method == 'POST':
+            return redirect(url_for('my_list'))  # Redirect to my_list after creating a new list
+        user_items_collection = db[first['_id']]  # Use the user's id as collection name
+        items = user_items_collection.find()  # Retrieve all items from the collection
+        return render_template('new_list.html', user=first, items=items)
+    elif user:
+        if request.method == 'POST':
+            return redirect(url_for('my_list'))  # Redirect to my_list after creating a new list
+        user_items_collection = db[user['_id']]  # Use the user's id as collection name
+        items = user_items_collection.find()  # Retrieve all items from the collection
+        return render_template('new_list.html', user=user, items=items)
+    else:
+        return redirect(url_for('login_page'))
+    
     
 # create temporary collection
 
 @app.route('/my_list', methods=['GET'])
 def my_list():
     user = session.get('login_user')
-    if user:
+    first = session.get('first_login')
+    if first:
+        user_items_collection = db[first['_id']]  # Use the user's id as collection name
+        items = user_items_collection.find()
+        return render_template('my_list.html', items=items)
+    elif user:
         user_items_collection = db[user['_id']]  # Use the user's id as collection name
         items = user_items_collection.find()
         return render_template('my_list.html', items=items)
@@ -151,7 +126,61 @@ def my_list():
         # Handle the case where the user is not logged in or does not exist
         return redirect('/login')
 
+
+@app.route('/get-items', methods=['GET'])
+def get_items():
+    user = session.get('login_user')
+    first = session.get('first_login')
+    if first:
+        user_items_collection = db[first['_id']]
+        items = list(user_items_collection.find())
+        # Convert ObjectId to string
+        for item in items:
+            item['_id'] = str(item['_id'])
+        return jsonify(items)
+    elif user:
+        user_items_collection = db[user['_id']]
+        items = list(user_items_collection.find())
+        # Convert ObjectId to string
+        for item in items:
+            item['_id'] = str(item['_id'])
+        return jsonify(items)
+    else:
+        return jsonify([]), 401
+
+
+#insert items to the DB to a collection called by the user id
+@app.route('/save-items', methods=['POST'])
+def save_items():
+    first = session.get('first_login')
+    login_user = session.get('login_user')
     
+    if first:
+        data = request.json
+        items = data.get('items')
+        if items:
+            user_items_collection = db[first['_id']]
+            user_items_collection.insert_many(items)
+            session.pop('first_login')
+            session['login_user'] = first
+            return redirect(url_for('my_list'))
+        
+    elif login_user:
+        data = request.json
+        current_items = list(db[login_user['_id']].find())
+        items = data.get('items')
+        if items:
+            user_items_collection = db[login_user['_id']]
+            if current_items:
+                db.drop_collection(user_items_collection)
+                user_items_collection = db[login_user['_id']]
+                user_items_collection.insert_many(current_items)
+            user_items_collection.insert_many(items)
+            return redirect(url_for('my_list'))
+    
+    return "Something went wrong"
+
+
 @app.route('/delete_item/<string:rowId>', methods=['DELETE'])
 def delete_item(rowId):
     user = session.get('login_user')
@@ -171,6 +200,6 @@ def logout():
     return redirect(url_for('home_page'))
 
 def create_app(app):
-    if __name__ == '__main__':
+    if __name__ == '_main_':
         port = int(os.environ.get('PORT', 5000))
         app.run(debug=True, host="0.0.0.0", port=port)
